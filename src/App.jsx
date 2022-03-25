58facbe62b005e21a511d10a4ec01f800fad5950
Ignacio Martin <ignaciolmartin@gmail.com>
2022-03-25 00:58:55 -0300

    add tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,11 @@ function App() {
 
   return (
     <div className="App">
+
+      <h1 className="text-3xl font-bold underline bg-red-500">
+        Hello world!
+      </h1>
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + React!</p>
